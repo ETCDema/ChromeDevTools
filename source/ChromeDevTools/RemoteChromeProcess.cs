cfa98ff7b91a8dbfc5e6b9a681b341a83af6cacf
Dema <etc.dema@gmail.com>
2024-08-27 23:39:32 +0300

    fix: Put method for new session

--- source/ChromeDevTools/RemoteChromeProcess.cs
+++ source/ChromeDevTools/RemoteChromeProcess.cs
@@ -1,54 +1,64 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Newtonsoft.Json;
 
 namespace MasterDevs.ChromeDevTools
 {
 	public class RemoteChromeProcess : IChromeProcess
-    {
-        private readonly HttpClient http;
+	{
+		private readonly HttpClient http;
 
-        public RemoteChromeProcess(string remoteDebuggingUri, TimeSpan? timeout = null)
-            : this(new Uri(remoteDebuggingUri), timeout)
-        {
+		public RemoteChromeProcess(string remoteDebuggingUri, TimeSpan? timeout = null)
+			: this(new Uri(remoteDebuggingUri), timeout)
+		{
 
-        }
+		}
 
-        public RemoteChromeProcess(Uri remoteDebuggingUri, TimeSpan? timeout = null)
-        {
-            RemoteDebuggingUri = remoteDebuggingUri;
+		public RemoteChromeProcess(Uri remoteDebuggingUri, TimeSpan? timeout = null)
+		{
+			RemoteDebuggingUri = remoteDebuggingUri;
 
 			http = new HttpClient
 			{
 				BaseAddress     = RemoteDebuggingUri,
 				Timeout         = timeout.HasValue ? timeout.Value : Timeout.InfiniteTimeSpan
-            };
-        }
+			};
+		}
 
-        public Uri RemoteDebuggingUri { get; }
+		public Uri RemoteDebuggingUri { get; }
 
-        public virtual void Dispose()
-        {
-            http.Dispose();
-        }
+		public virtual void Dispose()
+		{
+			http.Dispose();
+		}
 
-        public async Task<ChromeSessionInfo[]> GetSessionInfo()
-        {
-            string json = await http.GetStringAsync("/json");
-            return JsonConvert.DeserializeObject<ChromeSessionInfo[]>(json);
-        }
+		public async Task<ChromeSessionInfo[]> GetSessionInfo()
+		{
+			string json = await http.GetStringAsync("/json");
+			return JsonConvert.DeserializeObject<ChromeSessionInfo[]>(json);
+		}
 
-        public async Task<ChromeSessionInfo> StartNewSession()
-        {
-            string json = await http.GetStringAsync("/json/new");
-            return JsonConvert.DeserializeObject<ChromeSessionInfo>(json);
-        }
+		public async Task<ChromeSessionInfo> StartNewSession()
+		{
+			string json         = await PutAsyncAndGetString("/json/new", null);
+			return JsonConvert.DeserializeObject<ChromeSessionInfo>(json);
+		}
 
-        public async Task EndSession(ChromeSessionInfo si)
-        {
-            string json         = await http.GetStringAsync("/json/close/"+si.Id);
-        }
-    }
+		public async Task EndSession(ChromeSessionInfo si)
+		{
+			string json         = await http.GetStringAsync("/json/close/"+si.Id);
+		}
+
+		private async Task<string> PutAsyncAndGetString(string url, HttpContent? content)
+		{
+			var response		= await http.PutAsync(url, content).ConfigureAwait(false);
+			response.EnsureSuccessStatusCode();
+
+			return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+		}
+	}
 }
